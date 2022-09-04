6f1434adb794dd71a004755ab70efb1df0d005c7
dev global <pavankumar.s12459@gmail.com>
2022-09-04 14:27:57 +0530

    setup

--- src/Pages/HomePage/index.js
+++ src/Pages/HomePage/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import MainCarousel from '../../Components/MainCarousel';
 
 const HomePage = () => {
 	return (
 		<div>
-			<MainCarousel />
 			<h1>Basic Slider</h1>
 			<h1>Footer</h1>
 		</div>
