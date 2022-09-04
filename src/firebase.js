0deecd61a8acee0ae762bf746a586d05e699d8d8
dev global <pavankumar.s12459@gmail.com>
2022-09-04 19:42:29 +0530

    login with google functionality Added

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,8 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/storage';
+
 const firebaseConfig = {
 	apiKey: 'AIzaSyCHO2xIU4dFEMOgMxuTHADYX8QC0GzUbnk',
 	authDomain: 'hotstar-clone-ad729.firebaseapp.com',
@@ -10,7 +15,7 @@ const firebaseConfig = {
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
-const provider = firebase.auth.GoogleProvider();
+const provider = new firebase.auth.GoogleAuthProvider();
 const storage = firebase.storage();
 
 export { auth, provider, storage };
