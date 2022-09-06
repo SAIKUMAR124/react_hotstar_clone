95563a148b9dbdab3bf0d7ad6ecebef590d3db89
dev global <pavankumar.s12459@gmail.com>
2022-09-06 09:59:47 +0530

    Added main Carousel

--- src/Pages/HomePage/index.js
+++ src/Pages/HomePage/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import ImgSlider from '../../Components/ImgSlider';
+import './index.scss';
 
 const HomePage = () => {
 	return (
-		<div>
-			<h1>Basic Slider</h1>
-			<h1>Footer</h1>
-		</div>
+		<main
+			className='home-page'
+			style={{
+				background:
+					'url(images/home/home-background.png) center center / cover no-repeat fixed',
+			}}
+		>
+			<ImgSlider />
+		</main>
 	);
 };
 
